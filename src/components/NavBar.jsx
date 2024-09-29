fc83957194b01699c2f928ca261fd00a2a354f24
OlamideLanre <lanreprincess@gmail.com>
2024-09-29 15:11:04 +0100

    Responsive navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,14 +1,28 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "./NavStyle.css";
+import { useState } from "react";
 
 function NavBar() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
   return (
     <>
       <nav className="bg-[#020024] px-20 py-7 stroke">
         <div className="container mx-auto flex justify-between items-center">
-          <div className="text-white font-bold text-lg">SIGHTMATIC</div>
-          <ul className="flex space-x-8 text-white">
+          <div className="flex items-center">
+            <div className="text-white font-bold text-lg">SIGHTMATIC</div>
+            {/* Menu icon for toggling mobile menu */}
+            <div className="menu-icon ml-4" onClick={toggleMenu}>
+              &#9776; {/* Hamburger icon */}
+            </div>
+          </div>
+          <ul
+            className={`flex text-white navbar ${isMenuOpen ? "active" : ""}`}
+          >
             <li>
               <a to="/" className="text-white">
                 Home
@@ -40,6 +54,8 @@ function NavBar() {
                 Login/SignUp
               </a>
             </li>
+
+            {/* <li>menu</li> */}
           </ul>
         </div>
       </nav>
